8e4efa1244942fda6519001d7371d9d0b92994a0
ClassicVantage <61285209+ClassicVantage@users.noreply.github.com>
2020-10-27 17:36:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {StyleSheet, Text,View} from 'react-native'
+import HomeScreen from './screens/HomeScreen';
+
+export default function App(){
+  return(
+    <HomeScreen/>
+  )
+}
